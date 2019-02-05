57603ee3f818779cdffdc9315250f92a9854e2b9
Wei Lu <luwei.here@gmail.com>
2019-02-05 15:01:42 +0800

    Use es modules to organise scripts

---
+++ CS5346_A1_D3/index.js
@@ -0,0 +1,14 @@
+import q2 from './q2.js'
+import q3 from './q3.js'
+
+d3.csv("results.csv", function(d) {
+  return {
+    method : d.method,
+    bufSize : d.bufSize,
+    quality : +d.quality,
+    inefficiency : +d.inefficiency
+  }
+}).then(function(data) {
+  q2(data)
+  q3(data)
+})
