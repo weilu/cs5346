010d6db8e57ac34c558cac36fc55e5a6eda319dc
Wei Lu <luwei.here@gmail.com>
2019-02-06 00:04:18 +0800

    Separate box plot for each bufSize

--- CS5346_A1_D3/q2.js
+++ CS5346_A1_D3/q2.js
@@ -1,4 +1,6 @@
-// heavily refereced https://bl.ocks.org/bytesbysophie/0311395c1e082f98e67efaf2c7f9555b
+// box plot refereced https://bl.ocks.org/bytesbysophie/0311395c1e082f98e67efaf2c7f9555b
+// grouped plot refereced https://bl.ocks.org/mbostock/3887051
+
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
@@ -20,34 +22,16 @@ var xAxisBox = svg.append("g").attr("transform", "translate(40,0)");
 
 function render(groupCounts) {
   // Select all values into one Array for axis scaling (min/ max)
-  // and sort group counts so quantile methods work
   var globalCounts = [];
-  for (var key in groupCounts) {
-      var groupCount = groupCounts[key]
-      groupCounts[key] = groupCount.sort(sortNumber);
-      groupCounts[key].forEach(element => {
-          globalCounts.push(element);
-      });
+  for (var method in groupCounts) {
+    for (var bufSize in groupCounts[method]) {
+      var groupCount = groupCounts[method][bufSize]
+      groupCount.forEach(element => {
+          globalCounts.push(element)
+      })
+    }
   }
 
-  // Prepare the data for the box plots
-  var plotData = [];
-  var colorIndex = 0.1;
-  var colorIndexStepSize = 0.08;
-  for (var [key, groupCount] of Object.entries(groupCounts)) {
-      var record = {};
-      var localMin = d3.min(groupCount);
-      var localMax = d3.max(groupCount);
-
-      record["key"] = key;
-      record["counts"] = groupCount;
-      record["quartile"] = boxQuartiles(groupCount);
-      record["whiskers"] = [localMax, localMin];
-      record["color"] = d3.interpolateInferno(colorIndex);
-
-      plotData.push(record);
-      colorIndex += colorIndexStepSize;
-  }
 
   // // Create Tooltips
   // var tip = d3.tip().attr('class', 'd3-tip').direction('e').offset([0,5])
@@ -66,11 +50,20 @@ function render(groupCounts) {
   //     });
   // svg.call(tip);
 
-  // Compute an ordinal xScale for the keys in plotData
-  var xScale = d3.scalePoint()
+  // method
+  var x0 = d3.scaleBand()
+    .rangeRound([0, width-margin.right])
+    .paddingInner(0.1)
     .domain(Object.keys(groupCounts))
-    .rangeRound([0, width])
-    .padding([3]);
+
+  var bufSizeKeys = Object.keys(Object.values(groupCounts)[0])
+  // bufSize
+  var x1 = d3.scaleBand()
+    .rangeRound([0, x0.bandwidth()])
+    .padding(0.5)
+    .domain(bufSizeKeys)
+  var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#6b486b", "#ff8c00"]);
 
   // Compute a global y scale based on the global counts
   var min = d3.min(globalCounts);
@@ -80,60 +73,90 @@ function render(groupCounts) {
     .range([height, 0])
     .nice()
 
-  // Setup the group the box plot elements will render in
-  var g = svg.append("g")
-    .attr("transform", "translate(20,0)");
+  // // Setup the group the box plot elements will render in
+  // var g = svg.append("g")
+  //   .attr("transform", "translate(20,0)");
 
-  // Draw the box plot vertical lines
-  var verticalLines = g.selectAll(".verticalLines")
-    .data(plotData)
-    .enter()
-    .append("line")
-    .attr("x1", d => { return xScale(d.key) + barWidth/2; })
-    .attr("y1", d =>  { return yScale(d.whiskers[0]); })
-    .attr("x2", d =>  { return xScale(d.key) + barWidth/2; })
-    .attr("y2", d => { return yScale(d.whiskers[1]); })
-    .attr("stroke", boxPlotColor)
-    .attr("stroke-width", 1)
-    .attr("stroke-dasharray", "5,5")
-    .attr("fill", "none");
+  function makeRecord(groupCount) {
+    var record = {};
+    var localMin = d3.min(groupCount);
+    var localMax = d3.max(groupCount);
+
+    record["key"] = method + '-' + bufSize;
+    record["counts"] = groupCount;
+    record["quartile"] = boxQuartiles(groupCount);
+    record["whiskers"] = [localMax, localMin];
+
+    return record
+  }
+
+  var data = []
+  for (method in groupCounts) {
+    var rowObj = { method: method }
+    for (var bufSize in groupCounts[method]) {
+      rowObj[bufSize] = makeRecord(groupCounts[method][bufSize])
+    }
+    data.push(rowObj)
+  }
+
+  function makePlotData(d) {
+    return bufSizeKeys.map(key => ({key, record: d[key]}))
+  }
+
+  var g = svg.append("g")
+    .attr("transform", "translate(40,0)")
+    .selectAll("g")
+    .data(data)
+    .enter().append("g")
+      .attr("transform", d => "translate(" + x0(d.method) + ",0)")
+
+  g.selectAll(".verticalLines")
+    .data(makePlotData)
+    .enter().append("line") // Draw the box plot vertical lines
+      .attr("x1", d => x1(d.key) + barWidth/2)
+      .attr("y1", d =>  yScale(d.record.whiskers[0]))
+      .attr("x2", d =>  x1(d.key) + barWidth/2)
+      .attr("y2", d => yScale(d.record.whiskers[1]))
+      .attr("stroke", boxPlotColor)
+      .attr("stroke-width", 1)
+      .attr("stroke-dasharray", "5,5")
+      .attr("fill", "none");
 
   // Draw the boxes of the box plot, filled in white and on top of vertical lines
-  var rects = g.selectAll("rect")
-    .data(plotData)
-    .enter()
-    .append("rect")
+  g.selectAll("rect")
+    .data(makePlotData)
+    .enter().append("rect")
     .attr("width", barWidth)
-    .attr("height", d => { return yScale(d.quartile[2]) - yScale(d.quartile[0]); })
-    .attr("x", d => { return xScale(d.key); })
-    .attr("y", d => { return yScale(d.quartile[0]); })
-    .attr("fill", d => { return d.color; })
-    .attr("stroke", boxPlotColor)
+    .attr("height", d => yScale(d.record.quartile[2]) - yScale(d.record.quartile[0]))
+    .attr("x", d => x1(d.key))
+    .attr("y", d => yScale(d.record.quartile[0]))
+    .attr("fill", d => z(d.key) )
     .attr("stroke-width", 1)
+    // .attr("stroke", boxPlotColor)
     // .on('mouseover', tip.show)
     // .on('mouseout', tip.hide);
 
   // Config for whiskers and median
   var horizontalLineConfigs = [
   {   // Top whisker
-      x1: d => { return xScale(d.key) },
-      y1: d => { return yScale(d.whiskers[0]) },
-      x2: d => { return xScale(d.key) + barWidth },
-      y2: d => { return yScale(d.whiskers[0]) },
+      x1: d => x1(d.key),
+      y1: d => yScale(d.record.whiskers[0]),
+      x2: d => x1(d.key) + barWidth,
+      y2: d => yScale(d.record.whiskers[0]),
       color: boxPlotColor
   },
   {   // Median
-      x1: d => { return xScale(d.key) },
-      y1: d => { return yScale(d.quartile[1]) },
-      x2: d => { return xScale(d.key) + barWidth },
-      y2: d => { return yScale(d.quartile[1]) },
+      x1: d => x1(d.key),
+      y1: d => yScale(d.record.quartile[1]),
+      x2: d => x1(d.key) + barWidth,
+      y2: d => yScale(d.record.quartile[1]),
       color: medianLineColor
   },
   {   // Bottom whisker
-      x1: d => { return xScale(d.key) },
-      y1: d => { return yScale(d.whiskers[1]) },
-      x2: d => { return xScale(d.key) + barWidth },
-      y2: d => { return yScale(d.whiskers[1]) },
+      x1: d => x1(d.key),
+      y1: d => yScale(d.record.whiskers[1]),
+      x2: d => x1(d.key) + barWidth,
+      y2: d => yScale(d.record.whiskers[1]),
       color: boxPlotColor
   }
   ];
@@ -142,16 +165,15 @@ function render(groupCounts) {
   for(var i=0; i < horizontalLineConfigs.length; i++) {
       var lineConfig = horizontalLineConfigs[i];
       var horizontalLine = g.selectAll(".whiskers")
-          .data(plotData)
-          .enter()
-          .append("line")
-          .attr("x1", lineConfig.x1)
-          .attr("y1", lineConfig.y1)
-          .attr("x2", lineConfig.x2)
-          .attr("y2", lineConfig.y2)
-          .attr("stroke", lineConfig.color)
-          .attr("stroke-width", 1)
-          .attr("fill", "none");
+          .data(makePlotData)
+          .enter().append("line")
+            .attr("x1", lineConfig.x1)
+            .attr("y1", lineConfig.y1)
+            .attr("x2", lineConfig.x2)
+            .attr("y2", lineConfig.y2)
+            .attr("stroke", lineConfig.color)
+            .attr("stroke-width", 1)
+            .attr("fill", "none");
   }
 
   // add the Y gridlines
@@ -170,7 +192,7 @@ function render(groupCounts) {
       .call(yAxis);
 
   // Setup a series axis on the bottom
-  var xAxis = d3.axisBottom(xScale);
+  var xAxis = d3.axisBottom(x0);
   xAxisBox.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -186,16 +208,21 @@ function boxQuartiles(d) {
     ];
 }
 
-// Perform a numeric sort on an array
-function sortNumber(a,b) { return a - b; }
-
 export default function(allData) {
-  const dataByMethod = d3.nest()
+  const dataByMethodByBufSize = d3.nest()
     .key(d => d.method)
+    .key(d => d.bufSize)
     .object(allData)
-  for (const key in dataByMethod) {
-    dataByMethod[key] = dataByMethod[key].map(o => o.qoe)
+
+  // strip other fields
+  for (const method in dataByMethodByBufSize) {
+    for (const bufSize in dataByMethodByBufSize[method]) {
+      const stripped = dataByMethodByBufSize[method][bufSize].map(o => o.qoe)
+      // sort group counts so quantile methods work
+      dataByMethodByBufSize[method][bufSize] = stripped.sort((a, b) => a - b)
+    }
   }
-  render(dataByMethod)
+
+  render(dataByMethodByBufSize)
 }
 
