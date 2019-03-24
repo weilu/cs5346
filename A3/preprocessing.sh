fd92ddb1e70b75d30a738e5df996a9a73ba46a22
Wei Lu <luwei.here@gmail.com>
2019-03-24 11:55:27 +0800

    Preprocess multiple gz files
    As there are ver <50 ICSE entries in 00.gz

--- A3/preprocessing.sh
+++ A3/preprocessing.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+
 ## Download & unzip data
-# wget https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/corpus-2019-01-31/s2-corpus-00.gz
+# wget https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/manifest.txt
+# wget -B https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/ -i manifest.txt
 # gzip -d s2-corpus-*.gz
 
 ## Extract venue arXiv and venue ICSE into their own json files
 VENUES="ArXiv ICSE"
 for VENUE in $VENUES;
 do
-  grep -r --exclude="*.gz" '"venue":"'$VENUE'"' s2-corpus-00 > "$VENUE.txt"
+  grep -r --exclude="*.gz" '"venue":"'$VENUE'"' s2-corpus-* > "$VENUE.txt"
   echo '[' > "$VENUE.json"
-  sed -e 's/s2-corpus-00:/,/' "$VENUE.txt" | tail -c +2 >> "$VENUE.json"
+  sed -E 's/s2-corpus-[[:digit:]]{2}:/,/g' "$VENUE.txt" | tail -c +2 >> "$VENUE.json"
   echo ']' >> "$VENUE.json"
 done
