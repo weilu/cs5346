7426baf99b5c383d696706ac086da9ec279193c5
Wei Lu <luwei.here@gmail.com>
2019-03-27 09:17:32 +0800

    Q3: ICSE Publication Top 10 Keyword Trend

--- A3/q3.js
+++ A3/q3.js
@@ -8,11 +8,60 @@ export default function(data) {
   plotData.push(['year'].concat(Object.keys(pubCountByYear).map(year => parseInt(year))))
   plotData.push(['publications'].concat(Object.values(pubCountByYear)))
 
-  var chart = c3.generate({
+  // basic line chart
+  c3.generate({
     bindto: '#q3',
     data: {
       columns: plotData,
       x: 'year'
+    }
+  })
+
+  // stacked area chart by topic
+  const expandedData = []
+  data.forEach(d => d.entities.forEach(function(keyword) {
+    const simplePaper = {
+      title: d.title,
+      year: d.year,
+      keyword: keyword
+    }
+    expandedData.push(simplePaper)
+  }))
+  const pubCountByTopic = d3.nest()
+    .key(d => d.keyword)
+    .rollup(v => v.length)
+    .object(expandedData)
+
+  const top10Topics = Object.keys(pubCountByTopic)
+    .sort((a, b) => pubCountByTopic[b] - pubCountByTopic[a])
+    .slice(0, 10)
+
+  const pubCountByYearByTopic = d3.nest()
+    .key(d => d.year)
+    .key(d => d.keyword)
+    .rollup(v => v.length)
+    .object(expandedData)
+
+  var stackedPlotData = []
+  top10Topics.forEach(function(topic) {
+    const counts = [topic]
+    for (const year in pubCountByYearByTopic) {
+      const count = pubCountByYearByTopic[year][topic] || 0
+      counts.push(count)
+    }
+    stackedPlotData.push(counts)
+  })
+  stackedPlotData.push(['year'].concat(Object.keys(pubCountByYearByTopic)))
+
+  c3.generate({
+    bindto: '#q3a',
+    data: {
+      columns: stackedPlotData,
+      x: 'year',
+      type: 'area-spline',
+      groups: [
+        top10Topics
+      ]
     }
   })
 }
