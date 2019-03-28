a12e4983ee13efe29527f8ada5d1037d08f142df
Wei Lu <luwei.here@gmail.com>
2019-03-28 23:47:46 +0800

    Q4a: author co-author network basic visualisation
    TODO: color clusters and tooltips

--- A3/index.js
+++ A3/index.js
@@ -1,10 +1,12 @@
 import q1 from './q1.js'
 import q2 from './q2.js'
 import q3 from './q3.js'
+import q4a from './q4a.js'
 
 d3.json("ArXiv.json").then(function(data) {
   q1(data)
   q2(data)
+  q4a(data)
 })
 
 d3.json("ICSE.json").then(function(data) {
