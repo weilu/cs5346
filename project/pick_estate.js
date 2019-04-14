ebe9f4716fb6442939e96c7769adbe202b675eba
julwrites <julian.teh@outlook.com>
2019-04-15 00:55:36 +0800

    Refactoring to return highlight and zoom operations, and attaching initMap to the window

--- project/pick_estate.js
+++ project/pick_estate.js
@@ -1,43 +1,5 @@
-function highlightRegion(geoXmlDoc, name) {
-  var highlightOptions = {
-    fillColor: '#FFFF00',
-    strokeColor: '#000000',
-    fillOpacity: 0.9,
-    strokeWidth: 10
-  };
-  var highlightLineOptions = {strokeColor: '#FFFF00', strokeWidth: 10};
-
-  if (!geoXmlDoc || !geoXmlDoc.placemarks) return;
-  for (var i = 0; i < geoXmlDoc.placemarks.length; i++) {
-    var placemark = geoXmlDoc.placemarks[i];
-    var poly;
-
-    if (placemark.polygon) {
-      poly = placemark.polygon;
-    } else if (placemark.polyline) {
-      poly = placemark.polyline;
-    }
-
-    if (placemark.name === name) {
-      if (poly) {
-        var rowElem = document.getElementById('row' + i);
-        if (rowElem) rowElem.style.backgroundColor = '#FFFA5E';
-        if (geoXmlDoc.placemarks[i].polygon) {
-          poly.setOptions(highlightOptions);
-        } else if (geoXmlDoc.placemarks[i].polyline) {
-          poly.setOptions(highlightLineOptions);
-        }
-      }
-    } else {
-      var rowElem = document.getElementById('row' + i);
-      if (rowElem) rowElem.style.backgroundColor = '#FFFFFF';
-      poly.setOptions(
-          {fillColor: '#0000FF', strokeColor: '#0000FF', fillOpacity: 0.3});
-    }
-  }
-}
-
-function initMap() {
+window.initMap =
+    function() {
   var map;
   map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(1.351616, 103.808053),
@@ -47,32 +9,8 @@ function initMap() {
 
   var filename =
       'https://raw.githubusercontent.com/weilu/cs5346/master/project/data/geo/kml/planningboundary.kml';
-  var geoXml = null;
+  var geoXmlDoc = null;
 
-  geoXml = new geoXML3.parser({
-    map: map,
-    singleInfoWindow: true,
-    afterParse: function(doc) {
-      var currentBounds = map.getBounds();
-      if (!currentBounds) currentBounds = new google.maps.LatLngBounds();
-      // Geodata handling goes here, using JSON properties of the doc object
-      var geoXmlDoc = doc[0];
-      if (!geoXmlDoc || !geoXmlDoc.placemarks) return;
-      for (var i = 0; i < geoXmlDoc.placemarks.length; i++) {
-        var placemark = geoXmlDoc.placemarks[i];
-        if (placemark.polygon) {
-          setHighlightHandler(geoXmlDoc, placemark.polygon, i);
-        }
-        if (placemark.polyline) {
-          setHighlightHandler(geoXmlDoc, placemark.polyline, i);
-        }
-      }
-    }
-  });
-  geoXml.parse(filename);
-}
-
-function setHighlightHandler(geoXmlDoc, poly, id) {
   var highlightOptions = {
     fillColor: '#FFFF00',
     strokeColor: '#000000',
@@ -81,6 +19,75 @@ function setHighlightHandler(geoXmlDoc, poly, id) {
   };
   var highlightLineOptions = {strokeColor: '#FFFF00', strokeWidth: 10};
 
+
+  var geoXml = new geoXML3.parser({
+    map: map,
+    singleInfoWindow: true,
+    afterParse: function(doc) {
+      var currentBounds = map.getBounds();
+      if (!currentBounds) currentBounds = new google.maps.LatLngBounds();
+      // Geodata handling goes here, using JSON properties of the doc object
+      geoXmlDoc = doc[0];
+      if (!geoXmlDoc || !geoXmlDoc.placemarks) return;
+      for (var i = 0; i < geoXmlDoc.placemarks.length; i++) {
+        var placemark = geoXmlDoc.placemarks[i];
+        if (placemark.polygon) {
+          setHighlightHandler(
+              geoXmlDoc, placemark.polygon, i, highlightOptions,
+              highlightLineOptions);
+        }
+        if (placemark.polyline) {
+          setHighlightHandler(
+              geoXmlDoc, placemark.polyline, i, highlightOptions,
+              highlightLineOptions);
+        }
+      }
+    }
+  });
+  geoXml.parse(filename);
+
+  var highlightRegion =
+      function(name) {
+    if (!geoXmlDoc || !geoXmlDoc.placemarks) return;
+    for (var i = 0; i < geoXmlDoc.placemarks.length; i++) {
+      var placemark = geoXmlDoc.placemarks[i];
+      var poly;
+
+      if (placemark.polygon) {
+        poly = placemark.polygon;
+      } else if (placemark.polyline) {
+        poly = placemark.polyline;
+      }
+
+      if (placemark.name === name) {
+        if (poly) {
+          var rowElem = document.getElementById('row' + i);
+          if (rowElem) rowElem.style.backgroundColor = '#FFFA5E';
+          if (geoXmlDoc.placemarks[i].polygon) {
+            poly.setOptions(highlightOptions);
+          } else if (geoXmlDoc.placemarks[i].polyline) {
+            poly.setOptions(highlightLineOptions);
+          }
+        }
+      } else {
+        var rowElem = document.getElementById('row' + i);
+        if (rowElem) rowElem.style.backgroundColor = '#FFFFFF';
+        poly.setOptions(
+            {fillColor: '#0000FF', strokeColor: '#0000FF', fillOpacity: 0.3});
+      }
+    }
+  }
+
+  var zoomRegion =
+      function(name) {
+    // TODO
+  }
+
+  return {highlight: highlightRegion, zoom: zoomRegion};
+}
+
+function setHighlightHandler(
+    geoXmlDoc, poly, id, highlightOptions, highlightLineOptions) {
   poly.setOptions(
       {fillColor: '#0000FF', strokeColor: '#0000FF', fillOpacity: 0.3});
 
