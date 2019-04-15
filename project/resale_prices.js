e48d375c5964a474872a403c57309b5752e73ef6
Wei Lu <luwei.here@gmail.com>
2019-04-15 21:09:29 +0800

    Fix price file indentation

--- project/resale_prices.js
+++ project/resale_prices.js
@@ -1,11 +1,11 @@
 import util from './utils.js'
 
 function parseDate(date){
-    var splitted = date.split('-');
-    var quarterEndMonth = splitted[1].charAt(1) * 3;
-    var quarterStartMonth = quarterEndMonth - 3;
-    var parseTime = d3.timeParse('%m %Y');
-    return parseTime(quarterStartMonth + ' ' + splitted[0]);
+  var splitted = date.split('-');
+  var quarterEndMonth = splitted[1].charAt(1) * 3;
+  var quarterStartMonth = quarterEndMonth - 3;
+  var parseTime = d3.timeParse('%m %Y');
+  return parseTime(quarterStartMonth + ' ' + splitted[0]);
 }
 
 export default function(data, town, flatType) {
@@ -20,48 +20,48 @@ export default function(data, town, flatType) {
     console.log(priceflatTypes.values() + ' does not contain ' + flatType)
   }
 
-     const priceInformation = data.filter(priceInfo => priceInfo.town === town && flatType.includes(priceInfo.flat_type)).map(d => ({ quarter: d.quarter, price: d.price }))
-     const timeXaxis = ['x'].concat(priceInformation.map(
-                           info=> parseDate(info.quarter)))
-     const priceYaxis= [flatType].concat(priceInformation.map(info=> info.price))
+  const priceInformation = data.filter(priceInfo => priceInfo.town === town && flatType.includes(priceInfo.flat_type)).map(d => ({ quarter: d.quarter, price: d.price }))
+  const timeXaxis = ['x'].concat(priceInformation.map(
+    info=> parseDate(info.quarter)))
+  const priceYaxis= [flatType].concat(priceInformation.map(info=> info.price))
 
 
-   var chart = c3.generate({
+  var chart = c3.generate({
 
-         bindto: `#${"prices"} .viz .top.leftviz`,
-         data: {
-             x: 'x',
-             columns: [
-                 timeXaxis,
-                 priceYaxis
-             ]
-         },
-         axis: {
-             x: {
-                label: 'Year',
-                 type: 'timeseries',
-                 tick: {
-                     format: '%Y'
-                 }
-             },
-             y:{
-                label: 'Price(SGD)',
-                tick:{
-                    format: d3.format(".2s")
-                }
-             }
-         },
-         tooltip: {
-             format: {
-                 title: function (d) { 
-                   const date = new Date(d)
-                   const quarter = Math.floor((date.getMonth() + 3) / 3)
-                   const year = date.getFullYear()
-                   return `${year} Q${quarter}`
-                 },
-                 value: d => "$" + d3.format(",.0f")(d)
-             }
-         }
-     });
+    bindto: `#${"prices"} .viz .top.leftviz`,
+    data: {
+      x: 'x',
+      columns: [
+        timeXaxis,
+        priceYaxis
+      ]
+    },
+    axis: {
+      x: {
+        label: 'Year',
+        type: 'timeseries',
+        tick: {
+          format: '%Y'
+        }
+      },
+      y:{
+        label: 'Price(SGD)',
+        tick:{
+          format: d3.format(".2s")
+        }
+      }
+    },
+    tooltip: {
+      format: {
+        title: function (d) { 
+          const date = new Date(d)
+          const quarter = Math.floor((date.getMonth() + 3) / 3)
+          const year = date.getFullYear()
+          return `${year} Q${quarter}`
+        },
+        value: d => "$" + d3.format(",.0f")(d)
+      }
+    }
+  });
 
 }
